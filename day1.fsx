0d03443172e0e58c32422accbf2904f2dec6d5f0
neon-sunset <neon-sunset@outlook.com>
2024-12-01 13:39:39 +0200

    simplyify + speedup

--- day1.fsx
+++ day1.fsx
@@ -2,25 +2,21 @@ open System.IO
 
 let input =
     "day1.txt"
-    |> File.ReadLines
-    |> Seq.map (fun l -> int l[0..4], int l[8..])
-    |> Seq.toArray
+    |> File.ReadAllLines
+    |> Array.map (fun l -> int l[0..4], int l[8..])
     |> Array.unzip
+    ||> (fun l r -> Array.sort l, Array.sort r)
 
-let part1 =
-    let (lhs, rhs) = input
-    Seq.zip (Seq.sort lhs) (Seq.sort rhs)
-    |> Seq.map (fun (a, b) -> abs (a - b))
-    |> Seq.sum
+let part1 lhs rhs =
+    Seq.zip lhs rhs |> Seq.map (fun (a, b) -> abs (a - b)) |> Seq.sum
 
-let part2 =
-    let (lhs, rhs) = input
+let part2 lhs rhs =
     lhs
     |> Seq.countBy id
     |> Seq.map (fun (k, factor) ->
-        let freq = rhs |> Seq.filter (fun x -> x = k) |> Seq.length
+        let freq = rhs |> Array.filter (fun x -> x = k) |> Array.length
         k, (factor * freq))
     |> Seq.sumBy (fun (k, factor) -> k * factor)
 
-printfn "%d" part1
-printfn "%d" part2
+printfn $"Part1: {input ||> part1}"
+printfn $"Part2: {input ||> part2}"
